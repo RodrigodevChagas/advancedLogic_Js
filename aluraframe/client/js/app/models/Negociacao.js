dd46da440373c3a39454aca5736ff3eebb0907ef
RodrigoDevChagas <dyggo.ag22@gmail.com>
2022-06-09 14:14:51 -0300

    avançando na aula 1

---
+++ aluraframe/client/js/app/models/Negociacao.js
@@ -0,0 +1,13 @@
+class Negociacao{
+
+    constructor(data, quantidade, valor){
+        this._data = data;
+        this._quantidade = quantidade;
+        this._valor = valor;
+    }
+
+    obtemVolume(){
+        return this._quantidade * this._valor;
+    }
+
+    }
