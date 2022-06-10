985a1c05c9223587ee9483bd24e52d479bc446d7
RodrigoDevChagas <dyggo.ag22@gmail.com>
2022-06-10 02:15:05 -0300

    terminando a segunda etapa do curso

--- aluraframe/client/js/app/models/Negociacao.js
+++ aluraframe/client/js/app/models/Negociacao.js
@@ -1,13 +1,25 @@
 class Negociacao{
 
     constructor(data, quantidade, valor){
-        this._data = data;
+        this._data = new Date(data.getTime());
         this._quantidade = quantidade;
         this._valor = valor;
+
+        Object.freeze(this);
     }
 
-    obtemVolume(){
+    get volume(){
         return this._quantidade * this._valor;
+    }
+
+    get data(){
+        return new Date(this._data.getTime());
+    }
+    get quantidade(){
+        return this._quantidade;
+    }
+    get valor(){
+        return this._valor;
     }
 
     }
