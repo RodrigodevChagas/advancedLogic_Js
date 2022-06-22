337d0f34646e3a76ae9b6727ce81deb2831a85df
RodrigoDevChagas <dyggo.ag22@gmail.com>
2022-06-21 22:49:09 -0300

    avançando

--- aluraframe/client/js/app/helpers/DataHelper.js
+++ aluraframe/client/js/app/helpers/DataHelper.js
@@ -5,12 +5,14 @@ class DataHelper{
     }
 
     static dataParaTexto(data){
-        return data.getDate() 
-        + '/' + (data.getMonth() +1) 
-        + '/' + data.getFullYear();
+        return `${data.getDate()}/${data.getMonth() +1}/${data.getFullYear()}`;
     }
 
     static textoParaData(texto){
+        if(!/\d{4}-\d{2}-\d{2}/.test(texto)){
+            throw new Error('Deve estar no formato aaaa-mm-dd');
+        }
+        
         return new Date(...
             texto.split('-')
             .map((item, index) => item - index % 2)
--- aluraframe/client/js/app/models/Negociacao.js
+++ aluraframe/client/js/app/models/Negociacao.js
@@ -4,7 +4,6 @@ class Negociacao{
         this._data = new Date(data.getTime());
         this._quantidade = quantidade;
         this._valor = valor;
-
         Object.freeze(this);
     }
 
