d2c44db2bfe1971cc827ba645f816e8c45f5b708
RodrigoDevChagas <dyggo.ag22@gmail.com>
2022-06-24 00:59:13 -0300

    mensagem de confirmação

---
+++ aluraframe/client/js/app/views/MensagemView.js
@@ -0,0 +1,16 @@
+class MensagemView{
+    constructor(elemento){
+
+        this._elemento = elemento;
+    }
+
+    _template(model){
+
+        return model.texto ? `<p class="alert alert-info">${model.texto}</p>` : '<p></p>';
+    }
+
+    update(model){
+        
+        this._elemento.innerHTML = this._template(model);
+    }
+}
